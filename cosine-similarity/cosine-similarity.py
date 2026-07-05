966f72d628b1142b4cf3cab41487111199d23599
Pranav Reddy Mamidi <pranavreddy27.m@gmail.com>
2026-07-05 19:59:58 -0400

    Solve cosine-similarity

--- cosine-similarity/cosine-similarity.py
+++ cosine-similarity/cosine-similarity.py
@@ -1,19 +1,16 @@
 import numpy as np
 
 def cosine_similarity(a, b):
-    """
-    Compute cosine similarity between two 1D NumPy arrays.
-    Returns: float in [-1, 1]
-    """
-    # Write code here
     a = np.array(a)
     b = np.array(b)
-    
-    dot_product = np.dot(a,b)
-    cross_product = np.linalg.norm(a) * np.linalg.norm(b)
-    cosine_similarity = dot_product/cross_product
-    if cosine_similarity == 0 or cross_product==0:
-        return 0
-    else:
-        return float(cosine_similarity)
-    pass
+
+    dot = np.dot(a, b)
+    norm_a = np.linalg.norm(a)
+    norm_b = np.linalg.norm(b)
+
+    denom = norm_a * norm_b
+
+    if denom == 0:
+        return 0.0
+
+    return float(dot / denom)
